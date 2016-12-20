110543f3eb1dcc2f3cdabac1149408c627558beb
Sergio111 <sergallor@hotmail.com>
2016-12-20 19:10:40 +0100

    Volume ajusted

--- TorreRacing_Code/ModuleSceneIntro.cpp
+++ TorreRacing_Code/ModuleSceneIntro.cpp
@@ -19,7 +19,7 @@ bool ModuleSceneIntro::Start()
 	LOG("Loading Intro assets");
 	bool ret = true;
 
-	App->audio->PlayMusic("music/main.ogg");
+	App->audio->PlayMusic("music/main_theme.ogg");
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
