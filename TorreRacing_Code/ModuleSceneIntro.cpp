502a45d81ba627f3204ba3c0550f49b185e676b7
Sergio111 <sergallor@hotmail.com>
2016-12-20 17:53:09 +0100

    Music added

--- TorreRacing_Code/ModuleSceneIntro.cpp
+++ TorreRacing_Code/ModuleSceneIntro.cpp
@@ -19,6 +19,8 @@ bool ModuleSceneIntro::Start()
 	LOG("Loading Intro assets");
 	bool ret = true;
 
+	App->audio->PlayMusic("music/main.ogg");
+
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
